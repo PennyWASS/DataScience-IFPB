75d52c9db318d7c8ec02725d6abb465104133bc7
Wálisson Andrey <85573798+PennyWASS@users.noreply.github.com>
2022-09-03 22:22:23 -0300

    Início do projeto
    Co-Authored-By: Felipe <102631323+okamitk@users.noreply.github.com>

---
+++ Análise Airbnb - Rio de Janeiro/analise.py
@@ -0,0 +1,6 @@
+#Importando as bibliotecas:
+import pandas as pd
+import matplotlib as plt
+
+#Carregando a base de dados:
+df = pd.read_csv("http://data.insideairbnb.com/brazil/rj/rio-de-janeiro/2022-06-20/visualisations/reviews.csv")
